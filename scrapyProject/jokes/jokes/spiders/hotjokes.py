ff4ffcf8643bc21b7c4ae0fd14d262468c1a5338
XinuxC <305949617@qq.com>
2017-11-14 23:49:27 +0800

    project jokes

---
+++ scrapyProject/jokes/jokes/spiders/hotjokes.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from jokes.items import JokesItem
+
+class HotjokesSpider(scrapy.Spider):
+    name = "hotjokes"
+    allowed_domains = ["qiushibaike.com"]
+    start_urls = []
+
+    for i in range(1,3):
+        url = 'https://www.qiushibaike.com/8hr/page/%s/' % i
+        start_urls.append(url)
+
+    def parse(self, response):
+        item = JokesItem()
+
+        jokes = response.xpath('//*[@id="content-left"]/div')
+
+        for joke in jokes:
+            item['auther'] = joke.xpath('.//h2/text()').extract()[0]
+            item['content'] = joke.xpath('.//div[@class="content"]/span/text()').extract()
+            yield item
+
