34352d68eb009a4fe21d09a2e592d1d864bff841
XinuxC <305949617@qq.com>
2017-11-22 00:10:16 +0800

    itchat charts made

--- weChat/WeChatspider.py
+++ weChat/WeChatspider.py
@@ -7,93 +7,141 @@
 # @Date  : 2017/11/20
 # @Desc  :
 import os
-
 import re
+
 import itchat
 
 #login
 itchat.login()
-
-#send msg
-# friends = itchat.get_friends(update=True)
+# 获取朋友列表
+friends = itchat.get_friends(update=True)
+# send msg
 # itchat.send_image('D:\python\weChat\signature.jpg')
 
-#get all friends(including me) to caculate male/female percentage
-friends = itchat.get_friends(update=True)
-# for friend in friends:
-#     print(friend)
+#获取头像
+def save_headimg():
+    num = 1
+    for friend in friends:
+        img = itchat.get_head_img(friend['UserName'])
+        fileImage = open('headimg'+os.sep+str(num)+'.jpg','wb')
+        fileImage.write(img)
+        fileImage.close()
+        num += 1
 
-male = female = others = 0
-total = len(friends)
-for i in friends[:]:
-    sex = i['Sex']
-    if sex == 1:
-        male +=1
-    elif sex == 2:
-        female += 1
-    else:
-        others +=1
-print('男性 %.2f%%' % (float(male)/total*100))
-print('女性 %.2f%%' % (float(female)/total*100))
-print('其他 %.2f%%' % (float(others)/total*100))
+#生成照片墙
+import math
+def imaWall():
+    img_list = os.listdir('headimg')
+    size = int(math.sqrt(float(720*720)/len(img_list)))
+    lines = int(640/size)
+    image = Image.new('RGBA',(790,790))
+    x = y = 0
+    for i in range(1,len(img_list)+1):
+        try:
+            img = Image.open('headimg'+os.sep+str(i)+'.jpg')
+        except IOError:
+            print("error")
+        else:
+            img = img.resize((size,size),Image.ANTIALIAS)
+            image.paste(img,(x * size,y * size))
+            x += 1
+            if x == lines:
+                x = 0
+                y += 1
+    image.save('HeadsWall.jpg')
+
+import pyecharts
+#获取位置
+def get_location():
+    locations = []
+    for friend in friends[:]:
+        locations.append(friend['City'])
+    provinces = set(locations)
+    data = []
+    for i in provinces:
+        data.append((i,locations.count(i)))
+    print(data)
+    # data =[('江苏', 2), ('Ferrara', 1), ('北京', 28), ('黑龙江', 11), ('重庆', 1), ('上海', 8), ('Van', 1), ('湖南', 1), ('Antalya', 1), ('Barcelona', 1), ('Dubayy', 1), ('海南', 1), ('Male Atoll', 1), ('Victoria', 2), ('四川', 96), ('吉林', 2), ('North Thiladhunmathi', 1), ('Douro', 1), ('福建', 1), ('辽宁', 1), ('安徽', 1), ('St.Gallen', 1), ('广西', 1), ('Ontario', 1), ('New South Wales', 1), ('浙江', 1), ('Wong Tai Sin', 1), ('Fukuoka', 1), ('广东', 6), ('西藏', 1)]
+    # data =[('江苏', 2), ('北京', 28), ('黑龙江', 11), ('重庆', 1), ('上海', 8), ('湖南', 1),  ('海南', 1), ('四川', 96), ('吉林', 2), ('福建', 1), ('辽宁', 1), ('安徽', 1),  ('广西', 1),  ('浙江', 1),  ('广东', 6), ('西藏', 1)]
+    geo = pyecharts.Geo("朋友分布", "cordinates and counts", title_color="#fff", title_pos="center",
+              width=1200, height=600, background_color='#404a59')
+    attr, value = geo.cast(data)
+    geo.add("", attr, value,maptype='china', type="effectScatter",
+            visual_text_color="#fff",is_random=True, effect_scale=5, is_legend_show=False)
+    geo.render('location.html')
+
+#性别统计
+#性别conver to chart
+def to_charts():
+    male = female = others = 0
+    total = len(friends)
+    for i in friends[:]:
+        sex = i['Sex']
+        if sex == 1:
+            male += 1
+        elif sex == 2:
+            female += 1
+        else:
+            others += 1
+    attr = ["男", "女", "妖"]
+    v1 = [round(float(male)/total*100,2), round(float(female)/total*100,2), round(float(others)/total*100,2)]
+    #条状图
+    # bar = pyecharts.Bar("性别统计")
+    # bar.add("male/female/others", attr, v1, is_stack=True)
+    # bar.render()
+    #饼图-圆环图
+    pie = pyecharts.Pie("比例图",title_pos='right')
+    pie.add("",attr,v1,radius=[40, 75], label_text_color=None, is_label_show=True,
+        legend_orient='vertical', legend_pos='left')
+    pie.render('Sex.html')
 
 #获取好友个性签名 做成词云
-signatures = []
-for i in friends:
-    # signature = i['Signature']
-    signature= i['Signature'].strip().replace("span","").replace("class","").replace("emoji","")
-    rep = re.compile("1f\d.+")
-    signature = rep.sub("",signature)
-    signatures.append(signature)
-# print(signature)
 import jieba
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, ImageColorGenerator
 from PIL import Image
 import numpy as np
-from    collections import Counter
-#拼接字符串
-text = ''.join(signatures)
-#jieba分词
-# wordlist = jieba.cut(text,cut_all=True)
-wordlist = jieba.cut(text)
-wl_space_split = " ".join(wordlist)
+from  collections import Counter
+def to_wl_pic():
+    signatures = []
+    for i in friends:
+        # signature = i['Signature']
+        signature= i['Signature'].strip().replace("span","").replace("class","").replace("emoji","")
+        rep = re.compile("1f\d.+")
+        signature = rep.sub("",signature)
+        signatures.append(signature)
+    # print(signature)
 
-#wordcloud词云
-background = np.array(Image.open(os.path.join('D:\Git\SpiderMan\weChat','20171120222644.jpg')))
-my_wordCloud = WordCloud(background_color="white",
-                         max_words = 5000,  # 词云显示最大词数
-                         mask = background,  # 设置背景
-                         max_font_size=100,  # 字体最大值
-                         random_state=42,
-                         font_path='D:\python\weChat\STXINGKA.TTF',  # 设置字体
-                         width=1000,
-                         height=860,  # 设置图片默认大小
-                         margin=2).generate(wl_space_split)
-image_color = ImageColorGenerator(background)
-plt.imshow(my_wordCloud.recolor(color_func=image_color))
-plt.figure()
-plt.imshow(my_wordCloud)
-plt.axis("off")
-plt.show()
-my_wordCloud.to_file('sgn.jpg')
+    #拼接字符串
+    text = ''.join(signatures)
+    #jieba分词
+    wordlist = jieba.cut(text,cut_all=True)
+    # wordlist = jieba.cut(text)
+    wl_space_split = " ".join(list(wordlist))
 
-
-
-#conver to chart
-# import echarts
-# chart = echarts.Echart(u'%s的微信好友性别比例'%(friends[0]['NickName']),'from WeChat')
-# chart.use(echarts.Pie('WeChat',
-#               [{'value':male,'name':u'男性 %.2f%%' % (float(male)/total*100)},
-#                {'value':female,'name':u'女性 %.2f%%' % (float(female)/total*100)},
-#                {'others':others,'name':u'人妖 %.2f%%' % (float(others)/total*100)}
-#               ],
-#               radius = ['50%','70%']
-#               ))
-# chart.use(echarts.Legend(['male','female','others']))
-# del chart.json['xAxis']
-# del chart.json['yAxis']
-# chart.plot()  #Mac os下可用
+    #wordcloud词云
+    background = np.array(Image.open(os.path.join('D:\python\SpiderMan\weChat','20171120223350.jpg')))
+    my_wordCloud = WordCloud(background_color="white",
+                             max_words = 5000,  # 词云显示最大词数
+                             mask = background,  # 设置背景
+                             max_font_size=100,  # 字体最大值
+                             random_state=42,
+                             font_path='D:\python\weChat\STXINGKA.TTF',  # 设置字体
+                             width=1000,
+                             height=860,  # 设置图片默认大小
+                             margin=2).generate(wl_space_split)
+    image_color = ImageColorGenerator(background)
+    plt.imshow(my_wordCloud.recolor(color_func=image_color))
+    plt.figure()
+    plt.imshow(my_wordCloud)
+    plt.axis("off")
+    plt.show()
+    my_wordCloud.to_file('sgnMost.jpg')
 
 
-
+if __name__ == '__main__':
+    save_headimg()
+    imaWall()
+    get_location()
+    to_wl_pic()
+    to_charts()
